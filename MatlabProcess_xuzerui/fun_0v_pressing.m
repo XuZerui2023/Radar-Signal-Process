d8e855048ac7fdd9d948d75fcd5d7a1b7d5c8dc2
Richard Xu <130325501+XuZerui2023@users.noreply.github.com>
2025-07-08 21:53:32 +0800

    Delete MatlabProcess_xuzerui directory

--- MatlabProcess_xuzerui/fun_0v_pressing.m
+++
@@ -1,7 +0,0 @@
-% 压制MTD_0速附近的波形，抑制静止目标、地杂波
-function MTD=fun_0v_pressing(MTD)%无噪回波幅度控制
-[prtNum,point_prt]=size(MTD);
-zero_v_pos=round(prtNum/2);
-% MTD(zero_v_pos-round(prtNum/150):zero_v_pos+round(prtNum/150),:)=mean(mean(abs(MTD)));
-MTD(zero_v_pos-round(prtNum/150):zero_v_pos+round(prtNum/150),:)=0;
-end
